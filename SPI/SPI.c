d10a53f73643c699d583376274010d0579a45138
IlyaM <carlogulliani@gmail.com>
2019-11-07 00:14:27 +0300

    Update SPI.c

--- SPI/SPI.c
+++ SPI/SPI.c
@@ -4,74 +4,56 @@
 
 void SPI1_INIT(void)
 {
-    SPI_InitTypeDef SPI_InitStructure;
-    GPIO_InitTypeDef GPIO_InitStructure;
-    NVIC_InitTypeDef NVIC_InitStructure;
+    GPIO_InitTypeDef GPIO_InitDef;
+    SPI_InitTypeDef SPI_InitDef;
 
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_SPI1, ENABLE);
+    // initialize init structs
+    GPIO_StructInit(&GPIO_InitDef);
+    SPI_StructInit(&SPI_InitDef);
 
-    /*配置 SPI_NRF_SPI的 SCK,MISO,MOSI引脚，GPIOA^5,GPIOA^6,GPIOA^7 */
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_5 | GPIO_Pin_6 | GPIO_Pin_7;
-    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_10MHz;
-    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP; //复用功能
-    GPIO_Init(GPIOA, &GPIO_InitStructure);
+    // initialize clocks
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_SPI1 | RCC_APB2Periph_AFIO | RCC_APB2Periph_GPIOA, ENABLE);
 
-    /*配置SPI_NRF_SPI的CE引脚，和SPI_NRF_SPI的 CSN 引脚:*/
-    //NRF_CE--PA12
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12;
-    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_10MHz;
-    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
-    GPIO_Init(GPIOA, &GPIO_InitStructure);
-    //NRF_CSN--PA4
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_4;
-    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_10MHz;
-    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
-    GPIO_Init(GPIOA, &GPIO_InitStructure);
+    // initialize A4/SS alternate function open-drain (50 MHz)
+    GPIO_InitDef.GPIO_Pin = GPIO_Pin_4 | GPIO_Pin_6;
+    GPIO_InitDef.GPIO_Mode = GPIO_Mode_AF_PP;
+    GPIO_InitDef.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_Init(GPIOA, &GPIO_InitDef);
 
-    SPI_CSN_H();
+    // initialize A5/SCK alternate function open-drain (50 MHz)
+    GPIO_InitDef.GPIO_Pin = GPIO_Pin_5 | GPIO_Pin_7;
+    GPIO_InitDef.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+    GPIO_InitDef.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_Init(GPIOA, &GPIO_InitDef);
 
-    SPI_InitStructure.SPI_Direction = SPI_Direction_2Lines_FullDuplex; //双线全双工
-    SPI_InitStructure.SPI_Mode = SPI_Mode_Slave; //主模式
-    SPI_InitStructure.SPI_DataSize = SPI_DataSize_8b; //数据大小8位
-    SPI_InitStructure.SPI_CPOL = SPI_CPOL_Low; //时钟极性，空闲时为低
-    SPI_InitStructure.SPI_CPHA = SPI_CPHA_1Edge; //第1个边沿有效，上升沿为采样时刻
-    SPI_InitStructure.SPI_NSS = SPI_NSS_Soft; //NSS信号由软件产生
-    SPI_InitStructure.SPI_BaudRatePrescaler = SPI_BaudRatePrescaler_8; //8分频，9MHz
-    SPI_InitStructure.SPI_FirstBit = SPI_FirstBit_MSB; //高位在前
-    SPI_InitStructure.SPI_CRCPolynomial = 7;
+    //  initialize SPI slave
+    // for slave, no need to define SPI_BaudRatePrescaler
+    SPI_InitDef.SPI_Direction = SPI_Direction_2Lines_FullDuplex;
+    SPI_InitDef.SPI_Mode = SPI_Mode_Slave;
+    SPI_InitDef.SPI_DataSize = SPI_DataSize_8b; // 8-bit transactions
+    SPI_InitDef.SPI_FirstBit = SPI_FirstBit_MSB; // MSB first
+    SPI_InitDef.SPI_CPOL = SPI_CPOL_Low; // CPOL = 0, clock idle low
+    SPI_InitDef.SPI_CPHA = SPI_CPHA_1Edge; // CPHA = 1
+    SPI_InitDef.SPI_NSS = SPI_NSS_Soft; // use hardware SS
+    SPI_InitDef.SPI_BaudRatePrescaler = SPI_BaudRatePrescaler_64; // APB2 72/64 = 1.125 MHz
+    // SPI_InitDef.SPI_BaudRatePrescaler = SPI_BaudRatePrescaler_256; // APB2 72/256 = 0.28 MHz
+    // SPI_InitDef.SPI_BaudRatePrescaler = SPI_BaudRatePrescaler_16; // APB2 72/16 = 4.5 MHz
+    SPI_InitDef.SPI_CRCPolynomial = 7;
+    SPI_Init(SPI1, &SPI_InitDef);
 
-    /* Configure the Priority Group to 1 bit */
-    NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);
-
-    /* Configure the SPI interrupt priority */
-    NVIC_InitStructure.NVIC_IRQChannel = SPIx_IRQn;
-    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 1;
-    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
-    NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
-    NVIC_Init(&NVIC_InitStructure);
-
-    /* Initialize the FIFO threshold */
-    SPI_RxFIFOThresholdConfig(SPI1, SPI_RxFIFOThreshold_QF);
-
-    /* Enable the Rx buffer not empty interrupt */
     SPI_I2S_ITConfig(SPI1, SPI_I2S_IT_RXNE, ENABLE);
 
-    /* Enable the SPI Error interrupt */
-    SPI_I2S_ITConfig(SPI1, SPI_I2S_IT_ERR, ENABLE);
-
-    SPI_Init(SPI1, &SPI_InitStructure);
-    /* Enable SPI1 */
     SPI_Cmd(SPI1, ENABLE);
+    NVIC_EnableIRQ(SPI1_IRQn);
     printf("SPI bus init success...\r\n");
 }
 
 
 u8 SPI_RW(u8 dat)
 {
-    while (SPI_I2S_GetFlagStatus(SPI1, SPI_I2S_FLAG_TXE) == RESET);
-    SPI_I2S_SendData(SPI1, dat);
+//    while (SPI_I2S_GetFlagStatus(SPI1, SPI_I2S_FLAG_TXE) == RESET);
+//    SPI_I2S_SendData(SPI1, dat);
+
     while (SPI_I2S_GetFlagStatus(SPI1, SPI_I2S_FLAG_RXNE) == RESET);
     return SPI_I2S_ReceiveData(SPI1);
 }
