cd060b69d9060595a5ff7e0f99160b9d8a6d35d9
IlyaM <carlogulliani@gmail.com>
2019-11-07 00:15:08 +0300

    Update main.c

--- SPI/main.c
+++ SPI/main.c
@@ -1,16 +1,11 @@
-#include <stm32f10x_conf.h>
-#include "SPI.h"
 #include "stm32f10x.h"
+#include "stm32f10x_gpio.h"
+#include "stm32f10x_rcc.h"
+#include "stm32f10x_spi.h"
+#include "SPI.h"
 
-#define SH
-
-#ifdef OS_USE_SEMIHOSTING
-  extern void initialise_monitor_handles(void);
-#endif
-
-uint8_t aRxBuffer [10];
-uint8_t ubRxIndex = 0;
-uint8_t ubTxIndex = 0;
+uint8_t RxBuff[128];
+uint8_t idx = 0;
 
 static void SysTickConfig(void) {
   /* Setup SysTick Timer for 10ms interrupts  */
@@ -23,70 +18,40 @@ static void SysTickConfig(void) {
   NVIC_SetPriority(SysTick_IRQn, 0x0);
 }
 
-uint8_t NRF_Read_Reg(uint8_t reg) {
-    uint8_t reg_val;
-    SPI_CSN_L();
-    SPI_RW(reg);
-    reg_val = SPI_RW(0);
-    SPI_CSN_H();
+int main(void) {
+  initialise_monitor_handles();
 
-    return 	reg_val;
+  /* SysTick configuration ---------------------------------------------------*/
+  //SysTickConfig();
+
+  printf("Start\r\n");
+
+  SPI1_INIT();
+
+  while(1) {
+	  //SPI1_IRQHandler();
+  }
 }
 
-uint8_t NRF_Read_Buf(uint8_t reg, uint8_t *pBuf, uint8_t uchars) {
-    uint8_t i;
-    uint8_t status;
-    SPI_CSN_L();
-    status = SPI_RW(reg);
-    for(i=0; i<uchars; i++)
-    {
-        pBuf[i] = SPI_RW(0);
-    }
-    SPI_CSN_H();
-    return 	status;
+void print_buf(uint8_t length) {
+	char temp[length];
+	for (u8 i=0; i<length; i++) {
+		printf("0x%02x ", RxBuff[i]);
+		temp[i] = (char)RxBuff[i];
+	}
+	printf("\r\n");
+	printf("This is element 0: %s\n", temp);
+	idx = 0;
 }
 
 void SPI1_IRQHandler(void) {
-  /* SPI in Slave Tramitter mode--------------------------------------- */
-  if (SPI_I2S_GetITStatus(SPI1, SPI_I2S_IT_TXE) == SET) {
-    SPI_RW(TxBuffer[ubTxIndex++]);
-    if (ubTxIndex == 5)
-    {
-      /* Disable the Tx buffer empty interrupt */
-      SPI_I2S_ITConfig(SPI1, SPI_I2S_IT_TXE, DISABLE);
-    }
-  }
 
-  /* SPI in Slave Receiver mode--------------------------------------- */
-  if (SPI_I2S_GetITStatus(SPI1, SPI_I2S_IT_RXNE) == SET) {
-    NRF_Read_Buf(0x00, aRxBuffer, 5);
-    #ifdef OS_USE_SEMIHOSTING
-      printf("%02x\r\n", aRxBuffer);
-    #endif
-  }
+	for (u8 i=0; i<6; i++) {
+		RxBuff[idx] = SPI_RW(0xDD);
+		idx++;
+	}
 
-  /* SPI Error interrupt--------------------------------------- */
-  if (SPI_I2S_GetITStatus(SPI1, SPI_I2S_IT_OVR) == SET) {
-    uint8_t error = NRF_Read_Reg(0x00);
-    SPI_I2S_GetITStatus(SPI1, SPI_I2S_IT_OVR);
-  }
-}
-
-int main(void) {
-
-  #ifdef OS_USE_SEMIHOSTING
-    initialise_monitor_handles();
-  #endif
-
-  /* SPI configuration ------------------------------------------------------*/
-  SPI_Config();
-
-  /* SysTick configuration ---------------------------------------------------*/
-  SysTickConfig();
-
-  /* Configure the SysTick handler priority */
-  NVIC_SetPriority(SysTick_IRQn, 0x0);
-
-  SPI1_INIT();
-  for(;;) {}
+	print_buf(6);
+	idx = 0;
+	memset(RxBuff, 0, 4);
 }
