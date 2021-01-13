5e284a1c0580bcc51c19f31bb2e48891dfc0a795
IlyaM <carlogulliani@gmail.com>
2021-01-13 16:43:21 +0300

    Update app.js

--- qiwi/app.js
+++ qiwi/app.js
@@ -10,38 +10,41 @@
         $scope.auth = function(login, password) {
             
             $scope.debug += login + "/" + password + "\r\n";
-            var settings = {
-                url: "https://httpbin.org/post", //$scope.url_prefix + "https://cloudfort.izumfin.com/api/auth",
-                method: "POST",
-                headers: {
-                  "Content-Type": "application/json",
-                },
-                data: {request:{login:login,password:password}},
-              };
-
-              var data = $.param({
-                fName: "111",
-                lName: "2222"
-            });
-        
-            var config = {
-                headers : {
-                    'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'
-                }
-            }
+            // var settings = {
+            //     url: "https://httpbin.org/post", //$scope.url_prefix + "https://cloudfort.izumfin.com/api/auth",
+            //     method: "POST",
+            //     headers: {
+            //       "Content-Type": "application/json",
+            //     },
+            //     data: {request:{login:login,password:password}},
+            //   };
               
-              $http.post("https://httpbin.org/post", data, config)
-              .success(result => {
-                  console.log(result.data);
-                  $scope.debug += result.data.origin + "\r\n";
-              })
-              .error(error => {
-                $scope.debug += error + "\r\n";
-                  console.log('error', error);
-              });
+            //   $http.post("https://httpbin.org/post", data, settings)
+            //   .then(result => {
+            //       console.log(result.data);
+            //       $scope.debug += result.data.origin + "\r\n";
+            //   })
+            //   .catch(error => {
+            //     $scope.debug += error + "\r\n";
+            //       console.log('error', error);
+            //   });
+            var settings_auth = {
+				"url": "https://httpbin.org/post",
+				"method": "POST",
+				"timeout": 0,
+				"headers": {
+					"Content-Type": "application/json"
+				},
+				"data": JSON.stringify({"request":{"login":"dashboard_gar","password":"JJjhs7eejw"}}),
+			};
+
+			$.ajax(settings_auth).done(function (response) {
+                console.log(response.response.sessionID);
+                $scope.debug += response.response.sessionID + "\r\n";
+            }
         }
 
-        $scope.comissions_0 = "PREPARING1";
+        $scope.comissions_0 = "PREPARING4";
         $scope.debug = "";
         $scope.auth("dashboard_gar", "JJjhs7eejw"); // BG
         $scope.debug += "00000000000000000000000000000000000000000000000000000000000000000\r\n";
