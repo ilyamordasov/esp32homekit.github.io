0047022babd14506f1da0c235a465c24c961264f
IlyaM <carlogulliani@gmail.com>
2021-01-13 16:00:05 +0300

    Update app.js

--- qiwi/app.js
+++ qiwi/app.js
@@ -3,32 +3,32 @@
     'use strict';
  
     var app = angular.module('App', []);
-    app.controller('DATA', function($scope, $http) {
+    app.controller('DATA', function($scope, $sce, $http) {
 
         $scope.url_prefix = "https://dashboardfplus.herokuapp.com/";
 
         $scope.auth = function(login, password) {
             $scope.comissions_0 = "READY";
-            var settings = {
-                url: $scope.url_prefix + "https://cloudfort.izumfin.com/api/auth",
-                method: "POST",
-                headers: {
-                  "Content-Type": "application/json",
-                },
-                data: {request:{login:login,password:password}},
-              };
+            // var settings = {
+            //     url: $scope.url_prefix + "https://cloudfort.izumfin.com/api/auth",
+            //     method: "POST",
+            //     headers: {
+            //       "Content-Type": "application/json",
+            //     },
+            //     data: {request:{login:login,password:password}},
+            //   };
               
-              $http(settings)
-              .then(response => response.data.response)
-              .then(result => {
-                  $scope.debug += result.sessionID + "\r\n";
-                  if (login === "dashboard_gar") { $scope.sessionID_BG = result.sessionID; $scope.updateData_BG(); }
-                  else if (login === "dashboard") { $scope.sessionID_FC = result.sessionID; $scope.updateData_FC();}
-              })
-              .catch(error => console.log('error', error));
+            //   $http(settings)
+            //   .then(response => response.data.response)
+            //   .then(result => {
+            //       $scope.debug += result.sessionID + "\r\n";
+            //       if (login === "dashboard_gar") { $scope.sessionID_BG = result.sessionID; $scope.updateData_BG(); }
+            //       else if (login === "dashboard") { $scope.sessionID_FC = result.sessionID; $scope.updateData_FC();}
+            //   })
+            //   .catch(error => console.log('error', error));
         }
 
-        $scope.comissions_0 = "PREPARING2";
+        $scope.comissions_0 = "PREPARING";
         $scope.debug = "";
         //$scope.auth("dashboard_gar", "JJjhs7eejw"); // BG
         $scope.debug += "00000000000000000000000000000000000000000000000000000000000000000\r\n";
